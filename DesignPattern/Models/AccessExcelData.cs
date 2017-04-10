f1f76272a97a18ba234fd7340c1b86421e68bcd9
Iliev88 <iliev.ilya@yahoo.com>
2017-04-10 16:16:56 +0300

    Creating class for connecting project with excel file

---
+++ DesignPattern/Models/AccessExcelData.cs
@@ -0,0 +1,44 @@
+﻿using SeleniumDesignPatternsDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.OleDb;
+
+namespace DesignPattern.Models
+{
+    public class AccessExcelData
+    {
+        public static string TestDataFileConnection()
+        {
+            var path = ConfigurationManager.AppSettings["TestDataSheetPath"];
+
+            var fileName = "UserData.xlsx";
+
+            var connection = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;
+                                             Data Source = {0};
+                                             Extended Properties=Excel 12.0;", path + fileName);
+
+            return connection;
+        }
+
+        public static RegistrateUser GetTestData(string keyName)
+        {
+            using (var connection = new OleDbConnection(TestDataFileConnection()))
+            {
+                connection.Open();
+
+                var query = string.Format("select * from [DataSet$] where key = '{0}'", keyName);
+
+                var value = connection.Query<RegistrateUser>(query).FirstOrDefault();
+
+                connection.Close();
+
+                return value;
+            }
+        }
+    }
+}
