f12ad802793d3f1d5471596d23cc9a3829f97485
Iliev88 <iliev.ilya@yahoo.com>
2017-04-11 12:06:24 +0300

    Add "HDR=YES" in connection string, but other error is thrown

--- DesignPattern/Models/AccessExcelData.cs
+++ DesignPattern/Models/AccessExcelData.cs
@@ -20,7 +20,7 @@ namespace DesignPattern.Models
 
             var connection = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;
                                              Data Source = {0};
-                                             Extended Properties=Excel 12.0;", path + fileName);
+                                             Extended Properties=Excel 12.0 Xml; HDR=YES", path + fileName);
 
             return connection;
         }
